02de4de6f24d31f01cdfe6f709131cd49bca8f0c
KaffinPX <KaffinPX@gmail.com>
2024-08-06 15:27:28 +0300

    Better exporting route

--- src/hooks/useKaspian.ts
+++ src/hooks/useKaspian.ts
@@ -1,7 +1,7 @@
 import { useContext } from "react"
 import { KaspianContext } from "../contexts/Kaspian"
 
-export default function useKaspian () {
+export function useKaspian () {
   const context = useContext(KaspianContext)
 
   if (!context) throw new Error("Missing Kaspian context")
